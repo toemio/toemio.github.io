2eb8dadfe0449fdc5923f7183ad608ca290695a2
Tomio Mataka <tomiomataka@gmail.com>
2025-05-29 11:52:26 -0400

    adding in enemies

--- class_practice/mvp.js
+++ class_practice/mvp.js
@@ -16,7 +16,103 @@ const player = {
 
 const keys = {};
 
+function drawDoritoship(ctx, x, y, angle) {
+    ctx.beginPath();
+    ctx.strokeStyle = "rgba(64, 128, 128)";
+    ctx.lineWidth = 3;
+    ctx.fillStyle = "rgba(0, 255, 128)";
+    ctx.beginPath();    
+    ctx.moveTo(x, y);
+    ctx.lineTo(x + 15, y + 40);
+    ctx.lineTo(x + 3.25, y + 29.7);
+    ctx.lineTo(x, y + 32.95);
+    ctx.lineTo(x - 3.25, y + 29.7);
+    ctx.lineTo(x - 15, y + 40);
+    ctx.lineTo(x, y);
+    ctx.stroke();
+    ctx.fill();
+    drawDspurple(ctx, x, y, angle);
+}
 
+
+
+function drawDspurple(ctx, x, y, angle) {
+    ctx.beginPath();
+    ctx.strokeStyle = "rgba(0, 0, 255)";
+    ctx.lineWidth = 1;
+    ctx.fillStyle = "rgba(255 0 255)";
+    ctx.beginPath();
+    ctx.moveTo(x, y + 5);
+    ctx.lineTo(x + 3, y + 25.2);
+    ctx.lineTo(x, y + 28.45);
+    ctx.lineTo(x - 3, y + 25.2);
+    ctx.lineTo(x, y + 5);
+    ctx.moveTo(x - 3.25, y + 36);
+    ctx.lineTo(x - 6, y + 46);
+    ctx.lineTo(x - 11, y + 44);
+    ctx.moveTo(x + 3.25, y + 36);
+    ctx.lineTo(x + 6, y + 46);
+    ctx.lineTo(x + 11, y + 44);
+    ctx.stroke();
+    ctx.fill();
+}
+
+function drawJeremy(ctx, x, y, angle) {
+   ctx.beginPath();
+   ctx.strokeStyle = "rgba(64, 128, 128)";
+   ctx.lineWidth = 3;
+   ctx.fillStyle = "rgba(0, 255, 128)";
+   ctx.beginPath();
+   y = y + 10;
+   ctx.moveTo(x, y + 2);
+   ctx.lineTo(x + 16, y + 16);
+//   ctx.lineTo(x + 5, y + 17);
+//   ctx.lineTo(x + 5, y + 10);
+   ctx.lineTo(x, y + 8);
+//   ctx.lineTo(x - 5, y + 10);
+//   ctx.lineTo(x - 5, y + 17);
+//   ctx.lineTo(x - 13, y + 14);
+   ctx.lineTo(x - 16, y + 16);
+   ctx.lineTo(x, y + 2);
+   ctx.stroke();
+   ctx.fill();
+   drawJpurple(ctx, x, y, angle);
+}
+
+function drawJpurple(ctx, x, y, angle) {
+   ctx.beginPath();
+   ctx.strokeStyle = "rgba(0, 0, 255)";
+   ctx.lineWidth = 1;
+   ctx.fillStyle = "rgba(255, 0, 255)";
+   ctx.beginPath();
+   y = y + 10;
+   ctx.moveTo(x + 9, y + 2);
+   ctx.lineTo(x + 14, y + 6);
+   ctx.lineTo(x + 10, y + 9);
+   ctx.moveTo(x - 9, y + 2);
+   ctx.lineTo(x - 14, y + 6);
+   ctx.lineTo(x - 10, y + 9);
+   ctx.moveTo(x, y + 1);
+   ctx.lineTo(x + 7, y + 5);
+   ctx.lineTo(x + 7, y + 9);
+   ctx.lineTo(x, y + 12);
+   ctx.lineTo(x - 7, y + 9);
+   ctx.lineTo(x - 7, y + 5);
+   ctx.lineTo(x, y + 1);
+   ctx.stroke();
+   ctx.fill();
+}
+
+function drawEnemies(ctx, x, y) {
+	drawDoritoship(ctx, 50, 50);
+	drawJeremy(ctx, 100, 50);
+	drawDoritoship(ctx, 150, 50);
+	drawDoritoship(ctx, 250, 50);
+	drawDoritoship(ctx, 150, 100);
+	drawDoritoship(ctx, 100, 100);
+	drawJeremy(ctx, 200, 100);
+	drawJeremy(ctx, 300, 100);
+}
 
 function drawRect(x,y) {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -115,6 +211,8 @@ function animate() {
 //	drawScore();
 	drawPlayer();
 	movePlayer();
+	drawEnemies();
+	drawJeremys();
 	shootBullet();
 //    x = x + dx;
 //    y = y + dy;
@@ -133,8 +231,8 @@ function animate() {
 //        dy = dy * -1;
 //    }
     requestAnimationFrame(animate);
-
 }
+
 function handleKeyPress(w){
 	keys[w.key] = true;
 	console.log(w.key);
